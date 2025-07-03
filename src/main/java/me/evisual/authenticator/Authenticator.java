71ddb21b5d8c17748e37f5b404f538b1c25045ea
Dallin Lo Forte <legos@coastblue.net>
2025-07-02 20:17:46 -0400

    Initial Commit

---
+++ src/main/java/me/evisual/authenticator/Authenticator.java
@@ -0,0 +1,12 @@
+package me.evisual.authenticator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Authenticator extends JavaPlugin {
+
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Enabled!");
+    }
+}
